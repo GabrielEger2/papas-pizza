98684df37d75c151b6124e604cc9ee29e8fee2ad
Gabriel Eger <gabriel_eger01@hotmail.com>
2023-03-16 11:20:00 -0300

    Menu filter added
    I've added the option to filter between savoury and sweet options, as well to change the prices between the sizes

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -7,7 +7,7 @@ const Banner = () => {
     <div className=' pt-24 p-4 max-w-[1640px] mx-auto'>
       <div className='max-h-[600px] relative'>
         <div className='absolute w-full h-full text-papaswhite max-h-[600px] bg-papasblack/70 flex flex-col justify-center'>
-          <h1 className='p-8 text-3xl sm:text-4xl md:text-6xl lg:text-7xl font-bold'>Looking for a<ReactTyped className=' text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-bold' strings={['&nbsp;Pizza?', '&nbsp;Sensation?', '&nbsp;Flavor?', 'n Experience?']} typeSpeed={80} backSpeed={100} loop/></h1>
+          <h1 className='p-8 text-3xl sm:text-4xl md:text-6xl lg:text-7xl font-bold'>Looking for a<ReactTyped className=' text-3xl sm:text-4xl md:text-6xl lg:text-7xl font-bold' strings={['&nbsp;Pizza?', '&nbsp;Sensation?', '&nbsp;Flavor?', 'n Experience?']} typeSpeed={80} backSpeed={100} loop/></h1>
           <h1 className='px-8 text-3xl sm:text-4xl md:text-6xl lg:text-7xl font-bold'>You've come to</h1>
           <h1 className='px-8 text-3xl sm:text-4xl md:text-6xl lg:text-7xl font-bold'>&nbsp;&nbsp;the right <span className='text-papasred title-font'>Place!</span></h1>
         </div>
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,46 +2,64 @@ import React, { useState } from 'react'
 import { data } from '../data/PizzaData'
 
 const Menu = () => {
+  const [foods, setFoods] = useState(data);
+  const [activeFilter, setActiveFilter] = useState(null);
+  const [activeSize, setActiveSize] = useState('medium');
 
-const [foods, setFoods] = useState(data)
+  const filterType = (type) => {
+    if (type === activeFilter) {
+      // If the same button is clicked twice, remove the filter
+      setFoods(data);
+      setActiveFilter(null);
+    } else {
+      // Otherwise, filter the data based on the selected type
+      setFoods(
+        data.filter((item) => {
+          return item.type === type;
+        })
+      );
+      setActiveFilter(type);
+    }
+  };
+
+  const buttonClass = 'm-1 bg-transparent hover:bg-papasred text-papasblack font-semibold hover:text-papaswhite py-2 px-4 border border-papasred hover:border-transparent rounded';
+  const activeButtonClass = 'm-1 bg-papasred text-papaswhite font-semibold py-2 px-4 border border-papasred rounded';
 
   return (
     <div className='max-w-[1640px] m-auto px-4 py-20'>
-        <h1 className='text-papasred font-bold text-6xl text-center'>Best Pizzas in Town:</h1>
-        <div className='flex flex-col lg:flex-row justify-between py-8'>
-            <div>
-                <p className='m-4 text-2xl font-bold text-papasblack'>Choose a size:</p>
-                <div className=' py-2 m-4 flex justify-center flex-wrap'>
-                    <button className='m-1 bg-transparent hover:bg-papasred text-papasblack font-semibold hover:text-papaswhite py-2 px-4 border border-papasred hover:border-transparent rounded'>Small</button>
-                    <button className='m-1 bg-transparent hover:bg-papasred text-papasblack font-semibold hover:text-papaswhite py-2 px-4 border border-papasred hover:border-transparent rounded'>Medium</button>
-                    <button className='m-1 bg-transparent hover:bg-papasred text-papasblack font-semibold hover:text-papaswhite py-2 px-4 border border-papasred hover:border-transparent rounded'>Large</button>
-                </div>
-            </div>
-
-            <div className=' justify-center'>
-                <p className='m-4 text-2xl font-bold text-papasblack'>Choose a type:</p>
-                <div className=' py-2 flex justify-center flex-wrap'>
-                    <button className='m-1 bg-transparent hover:bg-papasred text-papasblack font-semibold hover:text-papaswhite py-2 px-4 border border-papasred hover:border-transparent rounded'>Savoury</button>
-                    <button className='m-1 bg-transparent hover:bg-papasred text-papasblack font-semibold hover:text-papaswhite py-2 px-4 border border-papasred hover:border-transparent rounded'>Sweet</button>
-                </div>
-            </div>
-
-
+      <h1 className='text-papasred font-bold text-6xl text-center title-font'>Best Pizzas in Town:</h1>
+      <div className='flex flex-col lg:flex-row justify-between py-8'>
+        <div>
+          <p className='m-4 text-2xl font-bold text-papasblack'>Choose a size:</p>
+          <div className=' py-2 m-4 flex justify-center flex-wrap'>
+            <button onClick={() => setActiveSize('small')} className={activeSize === 'small' ? activeButtonClass : buttonClass}>Small</button>
+            <button onClick={() => setActiveSize('medium')} className={activeSize === 'medium' ? activeButtonClass : buttonClass}>Medium</button>
+            <button onClick={() => setActiveSize('large')} className={activeSize === 'large' ? activeButtonClass : buttonClass}>Large</button>
+          </div>
         </div>
 
-        <div className='grid grid-cols-2 lg:grid-cols-4 gap-6 pt-4'>
-            {foods.map((item, index)=> (
-                <div key={index} className='border border-papasred shadow-lg bg-papaslightred hover:scale-105 duration-500 rounded-lg'>
-                    <img src={item.image} alt={item.name} className='w-full h-[300px] object-cover rounded-t-lg'/>
-                    <div className='flex justify-between px-2 py-4'>
-                        <p className='font-bold text text-lg'>{item.name}</p>
-                        <p>
-                            <span className='bg-papasred font-bold text-papaswhite p-2 rounded-lg text-lg'>${item.price.medium}</span>
-                        </p>
-                    </div>
-                </div>
-            ))}
-        </div> 
+        <div className=' justify-center'>
+          <p className='m-4 text-2xl font-bold text-papasblack'>Choose a type:</p>
+          <div className=' py-2 flex justify-center flex-wrap'>
+            <button onClick={() => filterType('savoury')} className={activeFilter === 'savoury' ? activeButtonClass : buttonClass}>Savoury</button>
+            <button onClick={() => filterType('sweet')} className={activeFilter === 'sweet' ? activeButtonClass : buttonClass}>Sweet</button>
+          </div>
+        </div>
+      </div>
+
+      <div className='grid grid-cols-2 lg:grid-cols-4 gap-6 pt-4'>
+        {foods.map((item, index) => (
+          <div key={index} className='border border-papasred shadow-lg bg-papaslightred hover:scale-105 duration-500 rounded-lg'>
+            <img src={item.image} alt={item.name} className='w-full h-[300px] object-cover rounded-t-lg' />
+            <div className='flex justify-between px-2 py-4'>
+              <p className='font-bold text text-lg'>{item.name}</p>
+              <p>
+              <span className='bg-papasred font-bold text-papaswhite p-2 rounded-lg text-lg'>${item.price[activeSize]}</span>
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
