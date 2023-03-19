c301dc5901499d9425e5283227747b21c80253da
Gabriel Eger <52424334+GabrielEger2@users.noreply.github.com>
2023-03-19 19:30:01 -0300

    Email form working
    I've added the emailJS lib to make the email work, additionally, I've created conditions to not send the email if the final user didn't fill in all the required pieces of information

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { MdOutlineBeenhere } from 'react-icons/md'
 import { compose, withProps } from "recompose"
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
+import emailjs from '@emailjs/browser';
 
 const MyMapComponent = compose(
   withProps({
@@ -14,16 +15,42 @@ const MyMapComponent = compose(
   withGoogleMap
 )((props) =>
   <GoogleMap
-    defaultZoom={19}
+    defaultZoom={20}
     defaultCenter={{ lat: 40.856744, lng: -73.931606}}
   >
     {props.isMarkerShown && <Marker position={{ lat: 40.856829, lng: -73.931875 }} />}
   </GoogleMap>
 )
-
-
-
+ 
 export default function Contact(){
+
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
+
+  
+  function sendEmail(e) {
+    e.preventDefault();
+
+    if(name === '' || email === '' || message === ''){
+      alert("Please fill up all the required fields")
+    }
+
+    const templateParams ={
+      from_name: name,
+      message: message,
+      email: email
+    }
+
+    emailjs.send(process.env.REACT_APP_SERVICE_ID, process.env.REACT_APP_TEMPLATE_ID, templateParams, process.env.REACT_APP_PUBLIC_KEY)
+    .then((response) => {
+      console.log("email enviado", response.status, response.text)
+      setName('')
+      setEmail('')
+      setMessage('')
+    })
+
+  }
 
   return (
     <section id="contact">
@@ -41,7 +68,8 @@ export default function Contact(){
                 </div>
             </div>
 
-            <form className=" lg:absolute lg:right-20 right-0 justify-center mx-auto z-20 w-full max-w-sm space-x-3 bg-opacity-75"> 
+            <form onSubmit={sendEmail}
+            className=" lg:absolute lg:right-20 right-0 justify-center mx-auto z-20 w-full max-w-sm space-x-3 bg-opacity-75"> 
             <div className="w-full max-w-2xl px-5 py-10 m-auto bg-papasivory rounded-lg shadow-lg">
                 <div className="mb-6 text-5xl font-bold text-center text-papasred title-font">
                 Contact us !
@@ -49,17 +77,20 @@ export default function Contact(){
                 <div className="grid max-w-xl grid-cols-2 gap-4 m-auto">
                 <div className="col-span-2 lg:col-span-1">
                     <div className="relative">
-                    <input type="text" id="contact-form-name" className="rounded-lg border-transparent flex-1 appearance-none border border-papasred w-full py-2 px-4 bg-white text-papasblack placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-papaslightred focus:border-transparent" placeholder="Name"/>
+                    <input onChange={(e) => setName(e.target.value)} value={name}
+                    type="text" id="contact-form-name" className="rounded-lg border-transparent flex-1 appearance-none border border-papasred w-full py-2 px-4 bg-white text-papasblack placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-papaslightred focus:border-transparent" placeholder="Name"/>
                     </div>
                 </div>
                 <div className="col-span-2 lg:col-span-1">
                     <div className="relative">
-                    <input type="text" id="contact-form-email" className="rounded-lg border-transparent flex-1 appearance-none border border-papasred w-full py-2 px-4 bg-white text-papasblack placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-papaslightred focus:border-transparent" placeholder="email/phone"/>
+                    <input onChange={(e) => setEmail(e.target.value)} value={email}
+                    type="text" id="contact-form-email" className="rounded-lg border-transparent flex-1 appearance-none border border-papasred w-full py-2 px-4 bg-white text-papasblack placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-papaslightred focus:border-transparent" placeholder="email/phone"/>
                     </div>
                 </div>
                 <div className="col-span-2">
                     <label className="text-papasblack" htmlFor="name">
-                    <textarea className="flex-1 w-full px-4 py-2 text-base text-papasblack placeholder-gray-400 bg-white border border-papasred rounded-lg appearance-none focus:outline-none focus:ring-2 focus:ring-papaslightred focus:border-transparent" id="order" placeholder="Enter your order" name="order" rows="5" cols="40">
+                    <textarea onChange={(e) => setMessage(e.target.value)} value={message}
+                    className="flex-1 w-full px-4 py-2 text-base text-papasblack placeholder-gray-400 bg-white border border-papasred rounded-lg appearance-none focus:outline-none focus:ring-2 focus:ring-papaslightred focus:border-transparent" id="order" placeholder="Enter your order" name="order" rows="5" cols="40">
                     </textarea>
                     </label>
                 </div>
